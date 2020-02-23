5d76473b88adc6c59db5b383f6833e066b7520b8
onlinux <onlinux@free.fr>
2020-02-24 00:17:56 +0100

    Add test

---
+++ test/listen.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import socket
+ 
+UDP_IP = ''
+UDP_PORT = 5000
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print data.strip()
